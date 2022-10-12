5815b574a5a416c118e7d20c80f1e1f626346146
Ricaldone-Gabriel <gigga@DESKTOP-D1LBEMK>
2022-10-12 22:26:11 +0200

    classe figure

--- src/progfiguregeometrichegui/figureGiometriche.java
+++ src/progfiguregeometrichegui/figureGiometriche.java
@@ -10,12 +10,67 @@ package progfiguregeometrichegui;
  * @author gabriel.ricaldone
  */
 public class figureGiometriche {
-    private int base;
-    private int altezza;
-    private int raggio;
-    private int lato1;
-    private int lato2;
-    private int lato3;
+    private double base;
+    private double altezza;
+    private double raggio;
+    private double lato1;
+    private double lato2;
+    private double lato3;
     
-    //sus22
+    figureGiometriche() {
+        base = 0;
+        altezza = 0;
+        raggio = 0;
+        lato1 = 0;
+        lato2 = 0;
+        lato3 = 0;
+    }
+    
+    public void setBase(double base) {
+        
+    }
+    
+    public void setAltezza(double altezza) {
+        
+    }
+    
+    public void setRaggio(double raggio) {
+        
+    }
+    
+    public void setLato1(double lato1) {
+        
+    }
+    
+    public void setLato2(double lato2) {
+        
+    }
+    
+    public void setLato3(double lato3) {
+        
+    }
+    
+    public double getBase() {
+        return base;
+    }
+    
+    public double getAltezza() {
+        return altezza;
+    }
+    
+    public double getRaggio() {
+        return raggio;
+    }
+    
+    public double getLato1() {
+        return lato1;
+    }
+    
+    public double getLato2() {
+        return lato2;
+    }
+    
+    public double getLato3() {
+        return lato3;
+    }
 }
